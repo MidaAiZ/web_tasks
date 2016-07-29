a568fcaaa695142c121c65c671397556f25f489a
吴明达 <1036570699@qq.com>
2016-07-29 21:39:29 +0800

    优化了界面布局和一些代码注释

--- F1/lunbo.js
+++ F1/lunbo.js
@@ -2,17 +2,32 @@
 $(document).ready(function(){
 
         $("#container").lunbo() //初始化  无默认参数
+                       .lunbo("setNumStyle")
                        .lunbo("startShow" ,2000)//设置自动播放，可以添加播放时间间隔参数，也可不添加，默认2秒
                        .lunbo("onSlide")    //设置屏幕滑动切换图片
-                       .lunbo("setNumStyle")
                         //设置当鼠标在图片上时停止轮播 移出时继续轮播↓
                        .hover(function(){$.fn.lunbo("stopShow")} , function(){$.fn.lunbo("startShow",2000)});
-        /**给按钮监听点击事件  播放上一张 下一张
+        /*给按钮监听点击事件  播放上一张 下一张
           * 之所以不对点击切换图片的事件进行封装是为了增加用户对插件的可控性
         */
         $(".left-btn").bind("click" , function(){$.fn.lunbo("prev")});
         $(".right-btn").bind("click" , function(){$.fn.lunbo("next")});
         $(".num-ul li").bind("mousemove" , function(){$.fn.lunbo("gotoImg",$(this).lunbo("getIndex")); });
+
+        //$("#container").lunbo("setNumStyle",{
+        //    "defaultsCSS" : {
+        //        "width" : "10px",
+        //        "height" : "10px",
+        //        "border-radius" : "0px",
+        //        "background" : "yellow"
+        //        //...其它你想更改或增加的样式
+        //    },
+        //    "activeCSS" : {
+        //        "border-radius" : "0px",
+        //        "background":"blue"
+        //        //...其它你想更改或增加的样式
+        //    }
+        //});
     });
 
         /**
