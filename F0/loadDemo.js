967f92106cd9b224a0fe155418e3637870b8aa26
吴明达 <1036570699@qq.com>
2016-07-31 21:12:53 +0800

    优化了显示/隐藏密码的方法，通过改变input的type实现

--- F0/loadDemo.js
+++ F0/loadDemo.js
@@ -3,14 +3,14 @@
  */
 function showCode(){
     if (this.forms.password.type="password") {
-        document.getElementById("password-container").innerHTML = "<input type=\"text\" id=\"password\" name=\"password\" maxlength='18' placeholder=\"  Password\" value=" + this.forms.password.value + ">"
+        document.getElementById("password-container").type="text";
         document.getElementById("click").innerHTML="<a href=\"javascript:hideCode()\" class=\"showPassword\"></a>"
         document.getElementById("show-text").innerHTML="Show";
     }
 }
 function hideCode(){
     if (this.forms.password.type="text") {
-        document.getElementById("password-container").innerHTML = "<input type=\"password\" id=\"password\" name=\"password\" maxlength='18' placeholder=\"  Password\" value=" + this.forms.password.value + ">"
+        document.getElementById("password-container").type="password";
         document.getElementById("click").innerHTML="<a href=\"javascript:showCode()\" class=\"hidePassword\"></a>"
         document.getElementById("show-text").innerHTML="Hide";
     }
