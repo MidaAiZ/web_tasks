dc16ace7ac8882c074c2963ee57c6889d68be036
吴明达 <1036570699@qq.com>
2016-08-09 02:33:35 +0800

    增添了表单验证规则 添加了表单验证成功和失败时的动画 f2基本完成 至于scss版的css预编译暂时没时间写

--- F2/index.js
+++ F2/index.js
@@ -2,18 +2,18 @@
  * Created by Sinner on 2016/8/8.
  */
 
-function onFocus(event) {
+function onFocus(event) {       //点击输入框周边或者输入框获取焦点，变大
     switch(event.target){
-        case document.getElementById("id-panel") :
+        case document.getElementById("id-panel") :      //id输入框
         case document.getElementById("email-id") :
         {
-            //添加动画版class属性
+            //添加动画class属性
             addClass(document.getElementById("label-id"),"change-label");
             addClass(document.getElementById("email-id"),"change-input");
             document.getElementById("email-id").focus();
         };break;
 
-        case document.getElementById("pwd-panel") :
+        case document.getElementById("pwd-panel") :     //密码输入框
         case document.getElementById("email-pwd") :
         {
             addClass(document.getElementById("label-pwd"),"change-label");
@@ -22,7 +22,7 @@ function onFocus(event) {
         };break;
     }
 }
-function onBlur(event) {
+function onBlur(event) {    //输入框失去焦点  这时如果输入框内有文字输入框不会变小 如果没文字输入框会变小
     switch(event.target){
         case document.getElementById("email-id") :
         {
@@ -42,8 +42,11 @@ function onBlur(event) {
     }
 }
 //绑定移动端触摸事件
-document.getElementById("label-id").addEventListener("touchstart",function(event) {onFocus(event)})
-document.getElementById("label-pwd").addEventListener("touchstart",function(event) {onFocus(event)})
+document.onload=function() {
+    document.getElementById("label-id").addEventListener("touchstart",function(event) {onFocus(event)})
+    document.getElementById("label-pwd").addEventListener("touchstart",function(event) {onFocus(event)})
+}
+
 //添加class属性
 function addClass(ele,newClass){
     if (ele.className.indexOf(" "+newClass) === -1) {   //防止重复添加
@@ -57,7 +60,41 @@ function removeClass(ele,oldClass){
     }
 }
 
-function onChange(event) {
-//            switchEffect
+function checkForm() {
+    var idCheckModal = document.getElementById("email-id").getAttribute("data");
+    var pwdCheckModal = document.getElementById("email-pwd").getAttribute("data");
+    var idValue = document.getElementById("email-id").value;
+    var pwdValue = document.getElementById("email-pwd").value;
+    if(check(idCheckModal,idValue) && check(pwdCheckModal,pwdValue)) {  //id输入框只允许输入数字，密码框允许数字 字母 下划线  故分别验证
+        var success = document.getElementById("submit-success");
+        var checkMark = document.getElementById("check-mark");
+        addClass(success,"success");
+        addClass(checkMark,"success");
+        // document.getElementById("form").submit(); //提交表单
+    } else {
+        var wrong = document.getElementById("submit-wrong");
+        addClass(wrong,"wrong")
+        setTimeout(function(){removeClass(wrong,"wrong")},3000);
+    }
+}
+function check(checkModal,str){     //表单验证 提供四种验证规则，只需要在input后面写上data属性即可
+
+    var onlyNumber = /^\d{6,20}$/;                      //只允许数字
+    var Num_Lower = /^[0-9a-z\_]{6,20}$/;               //允许数字 小写 下划线
+    var Num_Upper = /^[0-9A-Z\_]{6,20}$/;               //允许数字 大写 下划线
+    var Num_Lower_Upper = /^[0-9a-zA-Z\_]{6,20}$/;      //允许数字 小写大写下划线
+
+    switch (checkModal){
+        case "onlyNumber" : { return onlyNumber.test(str)}
+        case "Num_Lower" : { return Num_Lower.test(str) }
+        case "Num_Upper" : { return Num_Upper.test(str) }
+        case "Num_Lower_Upper" :    //数字 字母 下划线是默认验证方式
+        default : { return Num_Lower_Upper.test(str) };
+    }
+}
+function enter(event) {     //监听密码框的回车键
+    if(event.keyCode === 13) {
+        checkForm();
+    }
 }
 
